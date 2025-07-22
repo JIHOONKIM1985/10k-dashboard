ec413367923dd66213e48fe84a4b7d35f4fa12dc
JIHOONKIM1985 <icdikjh85@gmail.com>
2025-07-23 01:00:30 +0900

    fix: 불필요한 loadAdjustment import 및 코드 제거 (빌드 에러 수정)

--- src/components/InitFirestore.tsx
+++ src/components/InitFirestore.tsx
@@ -1,16 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
-import { loadAdjustment, loadUploadData } from "@/utils/firestoreService";
+import { loadUploadData } from "@/utils/firestoreService";
 
 export default function InitFirestore() {
   useEffect(() => {
-    // Firestore에서 보정치 불러오기
-    loadAdjustment().then((data) => {
-      if (data) {
-        localStorage.setItem("adjustment", JSON.stringify(data));
-      }
-    });
     // Firestore에서 업로드 데이터 불러오기
     loadUploadData().then((data) => {
       if (data) {
